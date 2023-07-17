4385805d451becf6c05f69bfec779230dc752310
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-07-17 20:45:58 +0500

    Add program file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Doviz
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("USD to UZS");
+            Console.Write("Enter Amount USD: ");
+            decimal USD = decimal.Parse(Console.ReadLine());
+            System.Console.WriteLine("Converting from USD to UZS...");
+            decimal UZS =  11607 * USD;
+            Console.WriteLine($"{USD} USD = {UZS} UZS");
+            
+
+        }
+    }
+}
